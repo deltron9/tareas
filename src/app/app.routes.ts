ed81ea9140c33a7f80f12fdf6b75c2b075d490a5
deltron9 <cristian_collante@hotmail.com>
2026-04-13 08:07:44 -0300

    add: ruteo con sobre mi y dashboard con respectivos estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,31 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Home } from './pages/home/home';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { AboutMe } from './pages/about-me/about-me';
-import { ErrorPage } from './pages/error-page/error-page';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Main } from './main/main';
+import { Dashboard } from './main/dashboard/dashboard';
+import { AboutMe } from './main/about-me/about-me';
+import { ErrorPage } from './main/error-page/error-page';
 
 export const routes: Routes = [
     {
         path: 'login',
-        component: Login       
+        component: Login
     },
-    {
-        path: 'home',
-        component: Home
+    {   path: 'register',
+        component: Register 
     },
     {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'Login',
         pathMatch: 'full'
     },
     {
-        path: 'dashboard',
-        component: Dashboard
-    },
-    {
-        path: 'about-me',
-        component: AboutMe
+        path: 'main',
+        component: Main,
+        children: [
+            { path: 'dashboard', component: Dashboard },
+            { path: 'about-me', component: AboutMe }
+        ],
     },
     {
         path: '**',
--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router'; // Solo estos dos
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './main.html',
-  styleUrl: './main.css',
+  styleUrl: './main.css'
 })
-export class Main {}
+export class Main { }
