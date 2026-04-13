970650c8341bab24e3c27815b23107071c87d747
deltron9 <cristian_collante@hotmail.com>
2026-04-13 05:55:02 -0300

    refactor: agregado de ruta vacia redireccionada a home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { AboutMe } from './pages/about-me/about-me';
+import { ErrorPage } from './pages/error-page/error-page';
 
 export const routes: Routes = [
     {
@@ -14,11 +15,21 @@ export const routes: Routes = [
         component: Home
     },
     {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
         path: 'dashboard',
         component: Dashboard
     },
     {
         path: 'about-me',
         component: AboutMe
+    },
+    {
+        path: '**',
+        component: ErrorPage
     }
+
 ];
