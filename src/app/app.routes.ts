d41e5577991d0bc94e8619e33bddbf456791a998
deltron9 <cristian_collante@hotmail.com>
2026-04-13 00:03:32 -0300

    add: añadido de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { AboutMe } from './pages/about-me/about-me';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login       
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: 'about-me',
+        component: AboutMe
+    }
+];
